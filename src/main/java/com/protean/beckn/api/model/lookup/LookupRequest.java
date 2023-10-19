f551d1c6bae0cc2c8f7151a395a201e4e9e089d0
rashmig <rashmig@proteantech.in>
2023-10-19 15:54:07 +0530

    Updated LookupRequest and LookupResponse beans

--- src/main/java/com/protean/beckn/api/model/lookup/LookupRequest.java
+++ src/main/java/com/protean/beckn/api/model/lookup/LookupRequest.java
@@ -1,5 +1,7 @@
 package com.protean.beckn.api.model.lookup;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -10,9 +12,10 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class LookupRequest {
+	@JsonProperty("subscriber_id")
 	private String subscriberId;
-	private String country;
-	private String city;
+	//private String country;
+	//private String city;
 	private String domain;
 	private String type;
 }
--- src/main/java/com/protean/beckn/api/model/lookup/LookupResponse.java
+++ src/main/java/com/protean/beckn/api/model/lookup/LookupResponse.java
@@ -10,23 +10,42 @@ import lombok.Data;
 public class LookupResponse implements Serializable {
 
 	private static final long serialVersionUID = 8260095980978686681L;
-
+	
+	@JsonProperty("subscriber_id")
 	private String subscriberId;
+	
 	private String country;
 	private String city;
 	private String domain;
+	
+	@JsonProperty("signing_public_key")
 	private String signingPublicKey;
+	
+	@JsonProperty("encr_public_key")
 	private String encrPublicKey;
+	
+	@JsonProperty("valid_from")
 	private String validFrom;
+	
+	@JsonProperty("valid_until")
 	private String validUntil;
+	
 	private String created;
 	private String updated;
 
 	// below fields are from older version
 	private String type;
+	
+	@JsonProperty("subscriber_url")
 	private String subscriberUrl;
+	
+	@JsonProperty("br_id")
 	private String brId;
+	
 	private String status;
-	@JsonProperty("ukId")
+	
+	@JsonProperty("unique_key_id")
 	private String uniqueKeyId;
+
 }
+
