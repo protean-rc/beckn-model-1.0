0d5c4f468cc438a83fbbe1a0ee303e060b516c40
RashmiG-Protean <rashmig@proteantech.in>
2024-11-08 16:09:26 +0530

    Ignored unwanted properties from JSON

--- src/main/java/com/protean/beckn/api/model/common/Country.java
+++ src/main/java/com/protean/beckn/api/model/common/Country.java
@@ -1,9 +1,12 @@
 package com.protean.beckn.api.model.common;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import lombok.Data;
 
 @Data
 public class Country {
+	@JsonIgnore
 	private String name;
 	private String code;
 }
--- src/main/java/com/protean/beckn/api/model/common/Location.java
+++ src/main/java/com/protean/beckn/api/model/common/Location.java
@@ -1,27 +1,39 @@
 package com.protean.beckn.api.model.common;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import lombok.Data;
 
 @Data
 public class Location {
+	@JsonIgnore
 	private String id;
+	@JsonIgnore
 	private Descriptor descriptor;
+	@JsonIgnore
 	private String map_url;
+	@JsonIgnore
 	private String gps;
+	@JsonIgnore
 	private String address;
+	@JsonIgnore
 	private City city;
+	@JsonIgnore
 	private String district;
+	@JsonIgnore
 	private State state;
 	private Country country;
+	@JsonIgnore
 	private String area_code;
+	@JsonIgnore
 	private Circle circle;
+	@JsonIgnore
 	private String polygon;
 	
-	@JsonProperty("3dspace")
+//	@JsonProperty("3dspace")
+	@JsonIgnore
 	private String threeDSpace;
-	
+	@JsonIgnore
 	private String rating;
 }
 
